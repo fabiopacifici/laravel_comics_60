8477e85f4377f9d1b63801d2502e650e5e366b5b
fbhood <mail@fabiopacifici.com>
2022-06-10 11:34:23 +0200

    implement comics page

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,51 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    $data = [
+        'name' => 'DC Comics',
+        'text' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti nemo a accusantium harum qui neque ab nobis accusamus enim alias.'
+    ];
+    return view('homepage', $data);
+})->name('home');
+
+Route::get('/characters', function () {
+    return 'Characters page';
+})->name('characters');
+
+Route::get('/comics', function () {
+    $comics = config('db.comics');
+
+    return view('comics', compact('comics'));
+})->name('comics');
+
+Route::get('/movies', function () {
+    return 'Movies page';
+})->name('movies');
+
+Route::get('/tv', function () {
+    return 'Tv page';
+})->name('tv');
+
+Route::get('/games', function () {
+    return 'Games page';
+})->name('games');
+
+Route::get('/collectibles', function () {
+    return 'Collectibles page';
+})->name('collectibles');
+
+Route::get('/videos', function () {
+    return 'Videos page';
+})->name('videos');
+
+Route::get('/fans', function () {
+    return 'Fans page';
+})->name('fans');
+
+Route::get('/news', function () {
+    return 'News page';
+})->name('news');
+
+Route::get('/shop', function () {
+    return 'shop page';
+})->name('shop');
